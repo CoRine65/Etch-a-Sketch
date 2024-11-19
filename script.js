bab45fd1beb7b73200fbc1217f5dc72392d770f0
CoRine65 <coderine65@gmail.com>
2024-11-19 02:48:28 -0800

    script.js: Implemented a for loop to create 256 square divs for the grid. stylesheet.css Styled the .container class. Styled and added flex in the .gridItems. Styled the .gridSquares

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const container = document.querySelector(".container");
 const gridItem = document.createElement("div");
 gridItem.classList.add("gridItem");
 container.appendChild(gridItem);
+
+//iteration to create 256 squares for the 16x16
+for(let i = 0; i < 256; i++){
+    let gridSquare = document.createElement("div");
+    gridSquare.classList.add("gridSquare");
+    gridItem.appendChild(gridSquare);
+}
