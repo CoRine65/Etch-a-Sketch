4ac80b0c393d5288e99e2b7bf8ff8bff286f4907
CoRine65 <coderine65@gmail.com>
2024-11-20 01:40:28 -0800

    sript.js - Implemented hover effect for gridSquare - Added darkening opacity effect on hover

--- script.js
+++ script.js
@@ -9,4 +9,25 @@ for(let i = 0; i < 256; i++){
     let gridSquare = document.createElement("div");
     gridSquare.classList.add("gridSquare");
     gridItem.appendChild(gridSquare);
+
+    gridSquare.interactionCount = 0;
+
+    gridSquare.addEventListener("mouseover", () =>{
+        gridSquare.style.backgroundColor = "lightblue";
+
+        gridSquare.interactionCount++;
+        let opacity = gridSquare.interactionCount * 0.1;
+        if (gridSquare.interactionCount >= 10) {
+            opacity = 1; // Fully dark after 10 interactions
+        }
+
+        // Apply the new opacity to the square
+        gridSquare.style.opacity = opacity;
+
+
+    })
+
+
 }
+
+
